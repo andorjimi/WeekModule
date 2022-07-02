cc2099e309fdc9374919f9cc6003e1ed9907a5fa
Jimi <50575362+andorjimi@users.noreply.github.com>
2022-07-02 21:48:15 +0800

    Initial Commit

---
+++ WeekModule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeekModule
+//
+//  Created by Jimi Knight on 02/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeekModule/WeekModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekModuleApp.swift
+//  WeekModule
+//
+//  Created by Jimi Knight on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeekModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
