109c578c39cb5044be35bde0f98c3a692eea7fa7
Jimi <50575362+andorjimi@users.noreply.github.com>
2022-07-09 00:37:02 +0800

    built first run at UI

--- WeekModule/ContentView.swift
+++ WeekModule/ContentView.swift
@@ -9,33 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var weekNumber: Int = 1
+    @State private var mouduleNumber: Array = [1,2,3,4,5,6,7,8]
     
+    // Colours
+    let cream = Color(red: 1.000, green: 0.929, blue: 0.855)
+    let blue = Color(red: 0.239, green: 0.698, blue: 1.000)
     
     var body: some View {
         ZStack{
-            Color(red: 0.239, green: 0.698, blue: 1.000)
+            blue
                 .ignoresSafeArea()
             // Title
             VStack{
                 Text("Week to Module")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
-            // Display box
+                // Display box
                 VStack{
-                    Text("Week (nm) is in Module (M)")
-                        .font(.headline)
-                    Text("""
-                            Week 1
-                            Week 2
-                            Week 3
-                            Week 4
-                            Week 5
-                        """)
-                    .font(.title)
+                    Text("Week \(weekNumber + 1) is in Module (M)")
+                        .font(.title2)
+                    Text("Week Numbers go here")
+                        .font(.headline.bold())
                 }
-                .frame(maxWidth: .infinity)
+                .frame(maxWidth: .infinity ,maxHeight: 250)
                 .padding(.vertical, 20)
-                .background(Color(red: 1.000, green: 0.929, blue: 0.855))
+                .background(cream)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 // Week Number
@@ -45,20 +43,45 @@ struct ContentView: View {
                         .foregroundColor(.white)
                     
                     // Add Picker here
+                    Picker("Week Number", selection: $weekNumber) {
+                        ForEach(1..<43) {
+                            Text("Week \($0)")
+                                .font(.title)
+                                .foregroundColor(.black)
+                            //                                    .background(.blue)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    .foregroundColor(.white)
+                    
+                    
+                    VStack {
+                        Text("Module Number")
+                        
+                        Picker("Module Number", selection: $mouduleNumber) {
+                            ForEach(mouduleNumber, id: \.self) {
+                                Text($0, format: .number)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .background(cream)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
                     
                 }
-            }
+                .padding()
+                
+            } // ZStack
             .padding()
-        
-        } // ZStack
-        
-        
-        
-    } // Body
-} // ContentView
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            
+            
+            
+        } // Body
+    } // ContentView
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
